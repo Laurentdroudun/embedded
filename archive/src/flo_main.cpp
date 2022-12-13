6d1ec05b5e70fb163b236177728b01795c0e7627
Florian Jerram <florian.jerram@ensta-bretagne.org>
2022-12-13 10:35:40 +0100

    mu et sigma

--- archive/src/flo_main.cpp
+++ archive/src/flo_main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <map>
 
 #include "fft_utils.h"
-#include "constant.h"
+#include "constants.h"
 
 #define SAMPLE_NUMBER 10
 
@@ -34,6 +36,7 @@ int readSample(ifstream &file) {
 
 int main() {
 	string name_au("../genres/hiphop/hiphop.00000.au");
+	cout<<name_au<<endl;
 	string name_csv("output.csv");
 
 	ifstream file_s;
@@ -44,16 +47,39 @@ int main() {
 
 	for (int i=0; i<6; i++) cout<<read32Bits(file_s)<<endl;
 
-	while (!file_s.eof()) {
-		std::vector<std::complex<double>> v;
-		double real; 
-		double imaginary = 0.;
-		for (int i=0; i<K; i++) {
-			for (int i=0; i<N; i++) {
-				real = readSample(file_s);
-			}
+	std::vector<float> mu_prev, sigma_prev;
+	for (int j=0; j<N; j++) mu_prev.push_back(0.);
+	for (int j=0; j<N; j++) sigma_prev.push_back(0.);
+
+	for (int i=0; i<K; i++) {
+		std::vector<Complex> x = {};
+		std::vector<float> mu_now = {}, sigma_now = {};
+
+		for (int j=0; j<N; j++) x.push_back(readSample(file_s));
+		ite_dit_fft(x);
+
+		for (int j=1; j<=N; j++) {
+			float a = ((j-1.)/j)*mu_prev[j-1];
+			float b = (1./j)*norm(x[j-1]);
+			mu_now.push_back(a+b);
 		}
+		for (int j=0; j<N; j++)	mu_prev[j] = mu_now[j];
+
+		auto c = sigma_prev[0];
+		auto s = norm(x[0]);
+		for (int j=2; j<=N; j++) {
+			s = s + norm(x[j-1]);
+			c = c + sigma_prev[j-1] + pow(j*norm(x[j-1])-s, 2)/(j*(j-1.));
+			sigma_now.push_back(sqrt(c/N));
+		}
+		for (int j=0; j<N; j++) sigma_prev[j] = sigma_now[j];
+
+		cout<<mu_now[10]<<" ; "<<sigma_now[100]<<endl;
 	}
+	
+	std::map<string, std::pair<std::vector<float>,std::vector<float>>> m = {}; //liste des coordonnées 
+	m[name_au] = {mu_prev, sigma_prev};
+
 }
 
 
@@ -78,4 +104,9 @@ pb : stockage !
 donc on caclul ces deux vecteurs de manières itératives
 
 voir wikipédia calcul itératif pour la mise à jour de la variance
+
+
+
+
+entrainer le modèle linear.svm et récupérer les poids
 */
