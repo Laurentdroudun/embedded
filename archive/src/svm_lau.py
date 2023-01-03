30d8dd2dd09473803cd2e47af9a6795428a3b08b
Laurent Droudun <laurent.droudun@gmail.com>
2023-01-03 08:42:36 +0100

    add svm_lau

---
+++ archive/src/svm_lau.py
@@ -0,0 +1,60 @@
+from sklearn import svm
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+import numpy as np
+from scipy import signal, fft
+from matplotlib import pyplot as plt
+import os
+
+path = "../genres/"
+filenames=[]
+dir_list=os.listdir(path)
+for dir_ in dir_list :
+	# print(file for file in os.listdir(path+dir_))
+	for file in os.listdir(path+dir_) :
+		filenames.append(path+dir_+"/"+file)
+filenames=np.array(filenames)
+
+def get_Xy(filename) :
+	data = ""
+	with open(filename, 'rb') as au:
+	    magic = au.read(4)
+	    dataoffset = int.from_bytes(au.read(4), byteorder='big', signed=True)
+	    datasize = int.from_bytes(au.read(4), byteorder='big', signed=True)
+	    print(magic, dataoffset, datasize)
+	    print("Offset : ", dataoffset)
+	    print("Size : ", datasize / (1 << 20), " MiB")
+	    au.seek(0)
+	    au.read(dataoffset)
+	    data = au.read(datasize)
+
+
+	d = []
+	for i in range(0, datasize, 2):
+	    d.append(int.from_bytes(data[i:i + 2], byteorder='big', signed=True))
+
+	plt.plot(d)
+	plt.show()
+
+
+
+	data = np.array(d, dtype='float')
+	print(data[0], data[-1])
+	print(data.shape)
+	fs = 22050
+	f, t, Zxx = signal.stft(data, 22050, nperseg=512, noverlap=256, window='hamming', return_onesided=False)
+	Z = np.abs(Zxx)
+	print(Z.shape)
+	print(Z)
+	m = np.mean(Z, axis=1)
+	e = np.std(Z, axis=1)
+
+	# print(m)
+	# print(e)
+get_Xy(filenames[450])
+# X=np.vstack(m,e)
+# y=np.array(["filename"])
+
+# clf=svm.LinearSVC()
+# clf.fit(X,y)
